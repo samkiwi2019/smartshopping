a0179f68cc7e8887fea6daaf7dbe52e09f47301d
Sam <sam.kiwi2019@gmail.com>
2020-11-10 17:38:42 +1300

    rc3 - update spider

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using FluentScheduler;
 using Microsoft.AspNetCore.Mvc;
 using Smartshopping.Data;
 using Smartshopping.Dtos;
