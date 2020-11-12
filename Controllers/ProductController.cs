5515d799cc1e0217571f04222434022ee35656ef
Sam <sam.kiwi2019@gmail.com>
2020-11-12 20:02:40 +1300

    test for json to a specific Csharp object

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using AngleSharp.Common;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Smartshopping.Data;
 using Smartshopping.Dtos;
@@ -100,6 +104,35 @@ namespace Smartshopping.Controllers
         {
             Spider.SpiderMaker.GetAJob();
             return Content("The Spider already got a job!");
+        }
+
+        [HttpPost("test")]
+        public async Task<ActionResult> Test()
+        {
+            // var q = obj.q !=null ? Convert.ToString(obj.q) : "";
+            // var currentPage = obj.currentPage !=null ? Convert.ToInt32(obj.currentPage): 1;
+            // var sort = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, int>>(Convert.ToString(obj.sort));
+            // var perPage = obj.perPage != null ? Convert.ToInt32(obj.perPage) : 10 ;
+            // var categoryId = obj.categoryId != null ? Convert.ToInt32(obj.categoryId) : 0;
+
+            string rawValue = await GetRawBodyStringAsync(Request);
+            var query = Newtonsoft.Json.JsonConvert.DeserializeObject<Query>(rawValue);
+            return NoContent();
+        }
+
+        public static async Task<string> GetRawBodyStringAsync(HttpRequest request, Encoding encoding = null)
+        {
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            using (StreamReader reader = new StreamReader(request.Body, encoding))
+                return await reader.ReadToEndAsync();
+        }
+
+        class Query
+        {
+            public string q { get; set; }
+            public Dictionary<string, int> sort { get; set; }
         }
     }
 }
